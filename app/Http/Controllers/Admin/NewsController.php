b7e1be6b1d2e98300591429f880eaff8412eccef
ren4t <ren4tab@yandex.ru>
2023-09-09 19:20:59 +0100

    homework3

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,27 +1,29 @@
 <?php
-declare(strict_types=1);
 
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use function view;
 
 class NewsController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        $news = $this->getNews();
+        return view('admin.news.index',['newsList' => $news]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('admin.news.create');
     }
 
     /**
@@ -63,5 +65,4 @@ class NewsController extends Controller
     {
         //
     }
-   
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\IndexControllerM;
-use App\Http\Controllers\HomeControllerM;
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Admin\IndexController as AdminController;
+use App\Http\Controllers\Admin\NewsController as AdminNewsController;
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,33 +23,16 @@ use Illuminate\Support\Facades\Route;
 
 //Route::view('/','welcome');
 
-Route::get('/',[HomeControllerM::class,'index']);
-Route::get('/admin',[IndexControllerM::class,'index']);
-Route::get('/test',[IndexControllerM::class,'test1']);
-Route::get('/test',[IndexControllerM::class,'test2']);
-
-Route::get('/hello/{name}', static function(string $name): string {
-    return " Hello,{$name}";
-});
-Route::get('/info', static function(): string {
-    return " information";
-});
-//Route::get('/news/{id}', static function(string $id): string {
-//    return " news #id{$id}";
-//});
-Route::get('/newsM', [NewsControllerM::class,'news']);
-Route::get('/newsM/{id}', [NewsControllerM::class,'newsOne']);
-
-Route::get('/',[NewsController::class, 'welcome'])
-        ->name('main');
-Route::get('/show_category_news/{id}', [NewsController::class, 'showCategoryNews'])
-        ->name('show.category.news')
-        ->where('id','\d+');
-
-Route::group(['prefix' => 'guest'], static function(){
-    Route::get('/news', [NewsController::class, 'index'])
-        ->name('news');
-    Route::get('/news/{id}/show', [NewsController::class, 'show'])
-        ->name('news.show')
-        ->where('id','\d+');
+Route::get('/news',[NewsController::class, 'index']) //'index' метод в классе NewsController
+        ->name('news.index');
+Route::get('news/{id}',[NewsController::class,'show']) // url news/{id}
+        ->where('id','\d+') // 'id','\d+' регулярное выражени говорит что 'id' должно быть числом 
+        ->name('news.show'); // 'news.show' имя роута
+Route::get('news/categories',[NewsController::class,'categories']) 
+        ->name('news.categories'); 
+Route::group(['prefix'=>'admin', 'as' => 'admin.'], function (){ // 'prefix'=>'admin' к url добавится префикс admin/
+//'as' => 'admin.'  для всех роутов будет префикс admin.
+    Route::get('/', AdminController::class)->name('index');
+    Route::resource('categories', AdminCategoryController::class);// url admin/categories/
+    Route::resource('news', AdminNewsController::class);// url admin/news/
 });
